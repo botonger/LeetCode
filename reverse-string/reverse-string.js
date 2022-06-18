8eba8e05ccb50ee2ad899b5d6bde5c3cf1f44ea1
botonger <99788738+botonger@users.noreply.github.com>
2022-06-18 10:14:33 +0900

    Time: 106 ms, Memory: 49.5 MB - LeetHub

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    for(let i=0;i<s.length/2;i++){
+    for(let i=0, len=s.length/2;i<len;i++){
         let tmp = s[i]
         s[i] = s[s.length-i-1]
         s[s.length-i-1] = tmp
