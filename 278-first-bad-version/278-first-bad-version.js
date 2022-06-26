c6ee19f6d173207f92d00c3c52bb77e9a9c9679b
botonger <99788738+botonger@users.noreply.github.com>
2022-06-27 00:38:31 +0900

    Time: 108 ms (18.54%), Space: 41.7 MB (66.17%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -18,8 +18,7 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        let start = 1
-        let end = n
+        let start = 1, end = n
         
         while (start < end){
             let mid = Math.floor((start+end)/2)
