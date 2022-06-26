2fca0685c090a6561082ce4aaea3e9a9c0d5de84
botonger <99788738+botonger@users.noreply.github.com>
2022-06-26 22:22:58 +0900

    Time: 133 ms (50.94%), Space: 51.7 MB (15.16%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let dict = {}
+    for (let i of nums){
+        if(i in dict) return true
+        else dict[i] = 1
+    }
+    return false
+    
+};
