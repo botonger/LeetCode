1d1f414e568e466ffd727cc31f14682d8e9a4695
botonger <99788738+botonger@users.noreply.github.com>
2022-06-26 22:27:55 +0900

    Time: 161 ms, Memory: 44.4 MB - LeetHub

---
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let count ={}
+    let result = []
+    
+    for(i of nums1){
+        if(count[i]) count[i]+= 1
+        else count[i] =1 
+    }
+    for(i of nums2){
+        if(count[i] && count[i]!=0) {
+            count[i] -= 1
+            result.push(i)
+        }
+    }
+    return result
+};
