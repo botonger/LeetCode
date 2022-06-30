04bcfbc81341f8ee5efdc870de9d4ea2a437ae6b
botonger <99788738+botonger@users.noreply.github.com>
2022-07-01 00:51:38 +0900

    Time: 122 ms, Memory: 44.7 MB - LeetHub

--- fizz-buzz/fizz-buzz.js
+++ fizz-buzz/fizz-buzz.js
@@ -6,11 +6,12 @@ var fizzBuzz = function(n) {
     let res = []
     
     for(let i=1;i< n+1;i++){
-        console.log(i)
-        if(i%3===0 && i%5===0) res.push("FizzBuzz")
-        else if(i%3===0) res.push("Fizz")
-        else if(i%5===0) res.push("Buzz")
-        else res.push(String(i))
+        let temp = ''
+        if(i%15===0) temp = "FizzBuzz"
+        else if(i%3===0) temp = "Fizz"
+        else if(i%5===0) temp = "Buzz"
+        
+        res.push(temp || String(i))
     }
     return res
 };
