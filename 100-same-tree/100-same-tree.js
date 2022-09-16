dedf1dea1899ce5c3d0badcd1288f0f2bab9175f
botonger <99788738+botonger@users.noreply.github.com>
2022-09-16 18:31:07 +0900

    Time: 107 ms (17.49%), Space: 42.3 MB (49.09%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -11,12 +11,9 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    console.log(p, q)
-    
+var isSameTree = function(p, q) {    
     if(p === null && q === null) return true;
-    if(p === null || q === null) return false;
-    if(p.val !== q.val ) return false;
+    if(p === null || q === null || p.val !== q.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
     
 };
